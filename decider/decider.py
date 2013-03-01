dd3267d21463331b6d49e2593be832b7d77f87b6
Alexander Kalinin <Alexander.Kalinin@PC416-9>
2013-03-01 17:40:08 +0800

    ????????? ???? ?????????

---
+++ decider/decider.py
@@ -0,0 +1,19 @@
+class Calculator:
+
+    def __init__(self, t_unit = object):
+        self.t_unit = t_unit
+    pass
+
+    def perform_calc(self, calc_name):
+        pass
+
+    def _param_assign(self):
+        pass
+
+class Decider:
+
+    def __init__(self):
+        pass
+
+    def compare_with_cong(self,param, conf):
+        pass
