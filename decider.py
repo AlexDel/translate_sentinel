e11250ee38537161a66e01260c9341a0d98f1d4a
verbalab <verbalab@veralab-laptop>
2013-03-06 17:22:03 +0800

    ?????????????? ?????? ?? ??????? ??????????

--- decider.py
+++ decider.py
@@ -3,11 +3,23 @@ import os, json
 
 class Decider:
     def __init__(self):
-        pass
+        self.params = json.loads(file.read(open(os.path.join(os.path.dirname(__file__),'config.json'),'r')))
 
-    def compare_with_cong(self,param, conf):
-        pass
+    def compare_with_conf(self,value):
 
+        min_value = self.params[value]['min']
+        max_value = self.params[value]['max']
 
-r = json.loads(file.read(open(os.path.join(os.path.dirname(__file__),'config.json'),'r')))
-print r['string_length']['max']
+        #Если нет максимального или минимального значения, то присваиваем бесконечность
+        if not min_value and min_value != 0:
+            min_value = float('-inf')
+
+        if not max_value and max_value != 0:
+            max_value = float('inf')
+
+        if value >= min_value and value <= max_value:
+            return True
+        else:
+            return False
+
+print Decider().params["Length_difference"]['min']
