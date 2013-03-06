dcb973258bd6160924abe0cd0829eb491de1a176
verbalab <verbalab@veralab-laptop>
2013-03-06 17:26:18 +0800

    ?????????????? ?????? ?? ??????? ??????????[1]

--- decider.py
+++ decider.py
@@ -5,10 +5,10 @@ class Decider:
     def __init__(self):
         self.params = json.loads(file.read(open(os.path.join(os.path.dirname(__file__),'config.json'),'r')))
 
-    def compare_with_conf(self,value):
+    def compare_with_conf(self,param):
 
-        min_value = self.params[value]['min']
-        max_value = self.params[value]['max']
+        min_value = self.params[param]['min']
+        max_value = self.params[param]['max']
 
         #Если нет максимального или минимального значения, то присваиваем бесконечность
         if not min_value and min_value != 0:
@@ -17,9 +17,7 @@ class Decider:
         if not max_value and max_value != 0:
             max_value = float('inf')
 
-        if value >= min_value and value <= max_value:
+        if param >= min_value and value <= max_value:
             return True
         else:
             return False
-
-print Decider().params["Length_difference"]['min']
