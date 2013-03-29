de491b1ad73956621c86bea6c717c9724d9dc1c0
Alexander Kalinin <Alexander.Kalinin@PC416-9>
2013-03-29 18:29:35 +0800

    ??????? ????????? ???? ?? ???????

--- abuser/abuser.py
+++ abuser/abuser.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 import re
 
-rus_mats = {
+rus_abuse = [
     u".*ху(й|и|я|е|ли|ле).*",
     u".*пи(з|с)д.*",
     u"бля.*",
@@ -17,7 +17,20 @@ rus_mats = {
     u"г(а|о)ндон.*",
     u".*залуп.*",
     u"г(а|о)вн.*"
+]
+
+abuse_dicts = {
+    'ru':rus_abuse
 }
+
+def list_regexp_checker(string,regexp_list):
+
+    for regexp in regexp_list:
+        if re.match(regexp,string):
+            return True
+            break
+
+    return False
 
 def prepare_text(tokens, lang):
     u'''
@@ -30,7 +43,6 @@ def prepare_text(tokens, lang):
             (u'x',u'х'),
             (u'o',u'о'),
         ]
-
 
         for i in range(len(tokens)):
             for r in replaces:
@@ -45,9 +57,11 @@ def collect_abuse_words(tokens, lang):
     данная функция полкчает список токенов и язык и с помощью регулярных выражений ищет маты в них
     возвращает список матов
     '''
-
     abuse_words = []
 
+    abuse_regexp = abuse_dicts[lang]
+    for t in tokens:
+        if list_regexp_checker(t,abuse_regexp):
+            abuse_words.append(t)
 
     return abuse_words
-
